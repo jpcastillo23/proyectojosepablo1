6fa3cc9c442a5858ebd365ae82afa4bc73e73366
josecastillo.basura@gmail.com <josecastillo.basura@gmail.com@2f5d8c4d-cca3-f3a6-57fe-f0d37342cca4>
2011-07-14 05:18:39 +0000

    Ahora estos estan patentados para que la gente tambien los pueda utilizar en sus programas

--- Radio.java
+++ Radio.java
@@ -46,11 +46,19 @@ public class Radio implements JoseCastillo {
 	//Metodo que cambia de On/Off y regresa la posicion 
 	public boolean power(){
 		estado =  !estado;
+		if (estado)
+			System.out.println("El Radio Ahora esta : Encendido");
+		else 
+			System.out.println("El Radio Ahora esta : Apagado");
 		return estado;
 	}
 	//Metodo que cambia la Emision y regresa la nueva Modulacion
 	public boolean cambio(){
 		emision = !emision;
+		if(emision)
+			System.out.println("Ahora su Emisora esta en FM");
+		else
+			System.out.println("Ahora su Emisora esta en AM");
 		return emision;
 	}
 	//Metodo Implementado que Avanza la frecuencia
@@ -60,10 +68,13 @@ public class Radio implements JoseCastillo {
 			if (this.emision){
 				if(frecfmsup>=(frecuenciafm+0.2))
 					this.frecuenciafm += 0.2;
+					System.out.println("Estacion: "+frecuenciafm+ "  FM ");
+					
 			}
 			else{
 				if(frecamsup>=(frecuenciaam+10))
 					this.frecuenciaam += 10;
+						System.out.println("Estacion: "+frecuenciaam+ "  AM ");
 			}
 		}
 	}
@@ -74,32 +85,43 @@ public class Radio implements JoseCastillo {
 			if (this.emision){
 				if(frecfminf<=(frecuenciafm-0.2))
 					this.frecuenciafm -= 0.2;
+					System.out.println("Estacion: "+frecuenciafm+ "  FM ");
 			}
 			else{ 
 				if(frecaminf<=(frecuenciaam-10))
 					this.frecuenciaam -= 10;
+					System.out.println("Estacion: "+frecuenciaam+ "  AM ");
 			}
 		}
 	}
 	//Metodo que recibe el numero local de memoria
 	//en donde se Almacenara la frecuencia y la mofulacion
 	public void store(int localidad){
+		
 		if (emision)
 			this.htz[localidad]=frecuenciafm;
 		else
 			this.htz[localidad]=frecuenciaam;
 		this.amsfm[localidad]=emision;
+		System.out.println("Se ha Almacenado la frecuencia"+this.htz[localidad]+" En Localidad: "+ localidad);
+		
+		
 	}
 	//Metodo Implementado que recibe el numero de boton
 	//el cual fue presionado, restaurando asi la frec y
 	//Modulaicon a la situaicon actual
 	public void select(int cualboton){
+		System.out.println("Ah seleccionado el boton: "+ cualboton);
+		
 		if(amsfm[cualboton])
 			this.frecuenciafm=htz[cualboton];
 		else
 			this.frecuenciaam= (int)(htz[cualboton]);
 		
 		this.emision=amsfm[cualboton];
+		System.out.println(toString());
+		
+		
 	}
 	// metodo Implementado que establece el radio como
 	//Apagado
@@ -133,6 +155,24 @@ public class Radio implements JoseCastillo {
 			this.frecuenciafm= frecfminf;
 		else
 			this.frecuenciaam= frecaminf;
+	}
+	/*
+	Clase que muestra el estado actual de la radio, tanto como emisoras
+	frecuencias, estado actual.
+	*/
+	public String toString(){
+		String muestra = "";
+		muestra += "+++++++++++++++++++++++++++++++++++++++++++++++++\n";
+		if (estado)
+			muestra += "Tu Radio esta: Encendido \n";
+		else
+			muestra += "Tu Radio esta: Apagado\n";
+		if (emision)
+			muestra += "Frecuencia:"+frecuenciafm+" FM\n";
+		else
+			muestra += "Frecuencia:"+frecuenciaam+" AM\n";
+		muestra += "+++++++++++++++++++++++++++++++++++++++++++++++++\n";
+		return muestra;
 	}
 	
 	
